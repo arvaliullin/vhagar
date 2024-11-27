f7aa4f9eb8e91d39ad71d3395aed80e0947b43b7
Artur Valiullin <ar.valiullin@yandex.ru>
2024-11-27 21:02:18 +0300

    repo: init project

---
+++ examples/helloworld/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/arvaliullin/vhagar-core/pkg/constants"
+)
+
+func main() {
+	fmt.Println(constants.MESSAGE)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/arvaliullin/vhagar
+
+go 1.22.0
+
+require github.com/arvaliullin/vhagar-core v0.0.0-20241127175937-a202f7c852a0 // indirect
